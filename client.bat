93a0379702e19aaedf91f6c5818d7ad195c31931
Bernd Roffmann <b.roffmann@t-online.de>
2012-01-17 17:26:34 +0100

    added usage of maven

--- client.bat
+++ client.bat
@@ -1,4 +1,3 @@
-ECHO ist eingeschaltet (ON).
 wsimport -keep -d gen -p de.tipit.server.client http://localhost:9000/analysis?wsdl
 wsimport -keep -d gen -p de.tipit.server.client http://localhost:9000/bet_result?wsdl
 wsimport -keep -d gen -p de.tipit.server.client http://localhost:9000/community_admin?wsdl
@@ -6,3 +5,4 @@ wsimport -keep -d gen -p de.tipit.server.client http://localhost:9000/meta_data_
 wsimport -keep -d gen -p de.tipit.server.client http://localhost:9000/rules_admin?wsdl
 wsimport -keep -d gen -p de.tipit.server.client http://localhost:9000/tourn_admin?wsdl
 wsimport -keep -d gen -p de.tipit.server.client http://localhost:9000/user_session?wsdl
+@pause
